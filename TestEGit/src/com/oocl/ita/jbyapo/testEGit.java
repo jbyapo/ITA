5f911a17247eb82a0ca130d363eed99de25261d4
yapojo <yapojo@ZHA-ITA003-W7.corp.oocl.com>
2017-07-31 15:16:14 +0800

    Test EGit

---
+++ TestEGit/src/com/oocl/ita/jbyapo/testEGit.java
@@ -0,0 +1,10 @@
+package com.oocl.ita.jbyapo;
+
+public class testEGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Jamming is on the house!");
+	}
+
+}
