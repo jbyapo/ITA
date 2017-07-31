f2eeaf39f0eee2cce34d1c07be51482a5d039444
yapojo <yapojo@ZHA-ITA003-W7.corp.oocl.com>
2017-07-31 15:43:48 +0800

    2nd Commit

--- TestEGit/src/com/oocl/ita/jbyapo/testEGit.java
+++ TestEGit/src/com/oocl/ita/jbyapo/testEGit.java
@@ -5,6 +5,8 @@ public class testEGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Jamming is on the house!");
+		System.out.println("Japo!");
+		
 	}
 
 }
