8faf713fb5f420ea2435662e3749bc0f06a9a35a
YAPOJO <YAPOJO@ZHA-ITA003-W7.corp.oocl.com>
2017-08-04 13:43:51 +0800

    Commit in 2nd Branch

--- TestEGit/src/com/oocl/ita/jbyapo/testEGit.java
+++ TestEGit/src/com/oocl/ita/jbyapo/testEGit.java
@@ -4,7 +4,7 @@ public class testEGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Jamming is on the house!");
+		System.out.println("Test EGit!");
 		System.out.println("Japo!");
 		
 	}
