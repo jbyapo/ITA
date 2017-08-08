fcf8ceb17ca444047c37317c1b8be5340aebc25e
YAPOJO <YAPOJO@ZHA-ITA003-W7.corp.oocl.com>
2017-08-08 19:58:05 +0800

    Day 7: JavaScript Assignment

---
+++ TestEGit/src/com/oocl/ita/yapo/ita/backup/Day7Assignment.java
@@ -0,0 +1,45 @@
+package com.oocl.ita.yapo.ita.backup;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Day7Assignment
+ */
+@WebServlet("/Day7Assignment")
+public class Day7Assignment extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Day7Assignment() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/JSAssignment1.html");
+		requestDispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
